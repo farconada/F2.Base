1563ba79f5450ecf323426e85c868d34f4e7e7bb
Fernando Arconada <fernando.arconada@gmail.com>
2011-11-05 18:47:57 +0100

    IfMobile viewHelper

---
+++ Classes/ViewHelpers/IfMobileViewHelper.php
@@ -0,0 +1,26 @@
+<?php
+namespace F2\Base\ViewHelpers;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 5/11/11
+ * Time: 18:37
+ * To change this template use File | Settings | File Templates.
+ */
+ class IfMobileHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractConditionViewHelper {
+
+	/**
+	 * renders <f:then> child if mobile is true, otherwise renders <f:else> child.
+	 *
+	 * @return string the rendered string
+	 * @api
+	 */
+	public function render() {
+        $isMobile = preg_match('/(iPhone|IEMobile|Windows CE|NetFront|PlayStation|PLAYSTATION|like Mac OS X|MIDP|UP\.Browser|Symbian|Nintendo|Android)/', $_SERVER['HTTP_USER_AGENT']);
+		if ($isMobile) {
+			return $this->renderThenChild();
+		} else {
+			return $this->renderElseChild();
+		}
+	}
+}
