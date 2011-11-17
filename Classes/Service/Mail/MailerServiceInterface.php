8d371e046d0bfe7fcc13b52c226f78e894e6ebab
Fernando Arconada <fernando.arconada@gmail.com>
2011-11-17 19:22:57 +0100

    Servicio de notificacion por mail

---
+++ Classes/Service/Mail/MailerServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace F2\Base\Service\Mail;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 17/11/11
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+interface MailerServiceInterface {
+    public function sendMail($aTo,$aFrom,$aSubject,$aBody);
+}
---
+++ Classes/Service/Mail/SwiftMailerService.php
@@ -0,0 +1,35 @@
+<?php
+namespace F2\Base\Service\Mail;
+use TYPO3\FLOW3\Annotations as FLOW3;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 17/11/11
+ * Time: 18:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Servicio de envio de Mails
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class SwiftMailerService implements  MailerServiceInterface {
+    /**
+	 * @FLOW3\Inject
+	 * @var \TYPO3\SwiftMailer\MailerInterface
+	 */
+	protected $mailer;
+
+    public function sendMail($aTo, $aFrom, $aSubject, $aBody) {
+        $message = new \TYPO3\SwiftMailer\Message();
+		$message->setFrom($aFrom)
+				->setTo($aTo)
+				->setSubject($aSubject)
+				->setBody($aBody)
+                ->setContentType("text/html");;
+		$this->mailer->send($message);
+    }
+
+
+}
