4d98eed26c137b0cc895d996c60126704586fa34
farconada <fernando.arconada@gmail.com>
2011-11-29 17:02:33 +0100

    creado GearmanMailer

---
+++ Classes/Exception/MailerException.php
@@ -0,0 +1,20 @@
+<?php
+namespace F2\Base\Exception;
+use TYPO3\FLOW3\Annotations as FLOW3;
+	/**
+	 * Created by JetBrains PhpStorm.
+	 * User: falcifer
+	 * Date: 5/09/11
+	 * Time: 17:24
+	 * To change this template use File | Settings | File Templates.
+	 */
+
+/**
+ * Excepcion relacionda con la aplicacion
+ *
+ * @package F2.Base
+ * @subpackage Exception
+ */
+class MailerException extends BaseException {
+
+}
